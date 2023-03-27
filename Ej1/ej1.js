93de2830bcd1ce41cd4ddc971b6d0869dba02faf
valenti94br <valenti94br@gmail.com>
2023-03-27 17:15:33 +0200

    ahora sí, está todo arreglado

--- Ej1/ej1.js
+++ Ej1/ej1.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const puerto = 3000;
 
-app.listen(puerto, () => {
-  console.log(`Servidor levantado en el puerto ${puerto}`);
+app.listen("8080", () => {
+  console.log("Servidor levantado en el puerto 8080");
 });
